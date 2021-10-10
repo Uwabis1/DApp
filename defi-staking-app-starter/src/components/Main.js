3a83644bb308f061ffeba137ad89b59d8920db7c
Uwabis <uwabis@yahoo.com>
2021-10-10 12:20:36 +0100

    sixth commit

--- defi-staking-app-starter/src/components/Main.js
+++ defi-staking-app-starter/src/components/Main.js
@@ -1,38 +1,43 @@
 import React, {Component} from 'react'
 import tether from '../tether.png'
-//import Airdrop from './Airdrop'
-
-
+import Airdrop from './Airdrop'
 
 class Main extends Component {
     render() {
-       
         return (
             <div id='content' className='mt-3'>
                 <table className='table text-muted text-center'>
                     <thead>
-                    <tr style={{color:'black'}}>
+                    <tr style={{color:'white'}}>
                         <th scope='col'>Staking Balance</th>
                         <th scope='col'>Reward Balance</th>
                     </tr>
                     </thead>
                     <tbody>
-                    <tr style={{color:'black'}}> 
+                        <tr style={{color:'white'}}> 
                             <td>{window.web3.utils.fromWei(this.props.stakingBalance, 'Ether')} USDT</td>
                             <td>{window.web3.utils.fromWei(this.props.rwdBalance, 'Ether')} RWD</td>
                         </tr>
                     </tbody>
                 </table>
                 <div className='card mb-2'style={{opacity:'.9'}}>
-                    <form className= 'mb-3'>
+                    <form 
+                    onSubmit={(event) => {
+                        event.preventDefault()
+                        let amount
+                        amount = this.input.value.toString()
+                        amount = window.web3.utils.toWei(amount, 'Ether')
+                        this.props.stakeTokens(amount)
+                    }}
+                    className='mb-3'>
                         <div style={{borderSpacing:'0 1em'}}>
-                        <label className='float-left' style={{marginLeft:'15px'}}><b>Stake Tokens</b></label>
-                        <span className='float-right' style={{marginRight:'8px'}}>
-                        Balance: {window.web3.utils.fromWei(this.props.tetherBalance, 'Ether')}
-                        </span>
+                            <label className='float-left' style={{marginLeft:'15px'}}><b>Stake Tokens</b></label>
+                            <span className='float-right' style={{marginRight:'8px'}}>
+                                Balance: {window.web3.utils.fromWei(this.props.tetherBalance, 'Ether')}
+                            </span>
                             <div className='input-group mb-4'>
                                 <input
-                                // ref={(input)=> {this.input = input} } 
+                                ref={(input)=> {this.input = input} } 
                                 type='text'
                                 placeholder='0'
                                 required />
@@ -44,9 +49,7 @@ class Main extends Component {
                                 </div>
                             </div>
                             <button type='submit' className='btn btn-primary btn-lg btn-block'>DEPOSIT</button>
-
                         </div>
-
                     </form>
                     <button 
                     type='submit'
@@ -57,11 +60,12 @@ class Main extends Component {
                     }}
                     className='btn btn-primary btn-lg btn-block'>WITHDRAW</button>
                     <div className='card-body text-center' style={{color:'blue'}}>
-                        AIRDROP 
+                    AIRDROP <Airdrop 
+                    stakingBalance={this.props.stakingBalance}
+                    decentralBankContract={this.props.decentralBankContract} />
                     </div>
                 </div>
             </div>
-            
         )
     }
 }
